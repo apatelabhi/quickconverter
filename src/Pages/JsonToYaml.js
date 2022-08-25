be052856d58cb4aeb7dad965a012bf5bfdeb1a2e
Abhishek <fabhishe@LAMU02DG6JZMD6R.uhc.com>
2022-08-25 15:42:50 -0400

    updated tab 1

--- src/Pages/JsonToYaml.js
+++ src/Pages/JsonToYaml.js
@@ -1,17 +1,55 @@
-import { Input, Button } from 'antd';
+import { Input } from 'antd';
 import React from 'react';
+import { useState, useEffect } from "react";
 const { TextArea } = Input;
 
+
 function JsonToYaml() {
-  return (
-    <>
-    <TextArea rows={4} />
-    <br />
-    <Button type="primary">Primary Button</Button>
-    <br />
-    <TextArea rows={4} placeholder="maxLength is 6" maxLength={6} />
-    </>
-  );
+    const [json, setJson] = useState("");
+    const [yaml, setYAML] = useState("");
+
+    useEffect(() => {
+        // storing input json
+        localStorage.setItem("json", JSON.stringify(json));
+    }, [json]);
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+
+        const YAML = require('json-to-pretty-yaml');
+
+        const data = YAML.stringify(json);
+        setYAML(data)
+        localStorage.setItem("yaml", data);
+
+        //alert(data)
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}> 
+                <TextArea
+                    rows={4}
+                    type="text"
+                    value={json}
+                    onChange={(e) => setJson(e.target.value)}
+                />
+
+                <br />
+                <input type="submit" value="JSON TO YAML"></input>
+
+                <br />
+                <TextArea rows={4} 
+                placeholder="maxLength is 6" 
+                maxLength={6} 
+                type="text"
+                value={yaml}
+                />
+
+            </form>
+
+        </>
+    );
 }
 
 export default JsonToYaml;
